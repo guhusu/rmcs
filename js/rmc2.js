560700ec3ef1f3d345c8540d535369040d025972
rain <guhusu@gmail.com>
2013-12-11 18:01:43 +0800

    update

--- js/rmc2.js
+++ js/rmc2.js
@@ -5,6 +5,10 @@ var RMC={
 		_PAGE_STORE:[],//頁的紀錄
 		_CORDOVA_STATUS:false,//cordova status
 		_PAGE_EVENT:{'hidebefore':{},'hide':{},'showbefore':{},'show':{}},//事件 
+		_TMP:'',//暫存
+		init:function(fun){
+			this._TMP=fun;
+		},
 		create:function(){
 			RMC._SW=$(window).width();
 			RMC._SH=$(window).height();
@@ -12,6 +16,7 @@ var RMC={
 			var FP=false;//footer position
 			
 			var i=0;
+			var hash=window.location.hash;
 			$('.page').each(function(){
 				var CH=RMC._SH;
 				var chf=false;
@@ -19,7 +24,7 @@ var RMC={
 				//$(this).addClass("display-none");
 				$(this).attr('class','page display-none');
 				$(this).css({'width':RMC._SW+'px','height':RMC._SH+'px','overflow':'hidden'});
-				if(i==1) {
+				if(hash=='' && i==1) {
 					RMC._NOWID=this.id;
 					RMC._PAGE_STORE.push(this.id);
 				}
@@ -55,6 +60,13 @@ var RMC={
 					}else{
 						content.css({'height':CH+'px','overflow':'auto'});
 					}
+				}else{
+					//上下頁面時使用
+					if(!content){
+						header.after('<div class="content" style="height:'+CH+'px;overflow:auto;"></div>');
+					}else{
+						content.css({'height':CH+'px','overflow':'auto'});
+					}
 				}
 				/*if(content){
 					if(chf){
@@ -65,8 +77,12 @@ var RMC={
 			});
 			//$('#'+RMC._NOWID).removeClass("display-none");
 			//$('#'+RMC._NOWID).addClass("display-show");
+			if(hash!='')
+			{
+				RMC._NOWID=hash;
+				RMC._PAGE_STORE.push(hash);
+			}
 			$('#'+RMC._NOWID).attr('class','page display-show');
-			
 			//設定事件
 			$('[tap]').each(function(){
 				$(this).swipe( {
@@ -81,12 +97,22 @@ var RMC={
 		},
 		changePage:function(id){RMC.changepage(id);},
 		changepage:function(id){
+			//隱藏前
+			if(RMC._PAGE_EVENT['hidebefore'][RMC._NOWID]!=undefined){
+				eval(RMC._PAGE_EVENT['hidebefore'][RMC._NOWID]+"();");
+			}
+			//顯示前
+			if(RMC._PAGE_EVENT['showbefore'][id]!=undefined){
+                eval(RMC._PAGE_EVENT['showbefore'][id]+"();");
+			}
 			window.location.hash=id;
 			this._PAGE_STORE.push(id);
+			var hide_id=RMC._NOWID;
 			var tmp='#'+RMC._NOWID;
 			//$('#'+RMC._NOWID).attr('class','page display-none');
 			$(tmp).attr('class','page fadeOut');
 			RMC._NOWID=id;
+			//var nc=$('#'+id).attr('data-css');alert(nc);
 			$('#'+id).attr('class','page slideInRight');
 			setTimeout(function(){
 				//alert(id);
@@ -94,23 +120,39 @@ var RMC={
 				//alert($('body').outerWidth(true));
 				$('#'+id).attr('class','page display-now'); 
 				//$(tmp).attr('class','page display-none');
+				//顯示後
+				if(RMC._PAGE_EVENT['show'][id]!=undefined){
+                    //alert(RMC._PAGE_EVENT['show'][id]);
+                    eval(RMC._PAGE_EVENT['show'][id]+"();");
+				}
+				//隱藏後
+				if(RMC._PAGE_EVENT['hide'][hide_id]!=undefined){
+                    eval(RMC._PAGE_EVENT['hide'][hide_id]+"();");
+				}
+				//alert(RMC._PAGE_EVENT['show']);
 			},1200);
 		},
 		backPage:function(){RMC.backpage();},
 		backpage:function(){
-			var tmp='#'+RMC._NOWID;
-			$(tmp).attr('class','page display-show');
-			//$('#'+RMC._NOWID).attr('class','page display-none');
-			this._PAGE_STORE.pop();
-			RMC._NOWID=this._PAGE_STORE[this._PAGE_STORE.length-1];alert(RMC._NOWID);
-			$('#'+RMC._NOWID).attr('class','page slideInLeft');
-			window.location.hash=RMC._NOWID;
-			setTimeout(function(){
-			//	$('#'+RMC._NOWID).attr('class','page display-now');
-				$(tmp).attr('class','page hidden');
-			},1200);
-			//var tmp=this._PAGE_STORE.pop();
-			//alert(tmp);
+			if(this._PAGE_STORE.length>1){
+				var tmp='#'+RMC._NOWID;
+				$(tmp).attr('class','page display-show');
+				//$('#'+RMC._NOWID).attr('class','page display-none');
+				this._PAGE_STORE.pop();
+				RMC._NOWID=this._PAGE_STORE[this._PAGE_STORE.length-1];//alert(RMC._NOWID);
+				$('#'+RMC._NOWID).attr('class','page slideInLeft');
+				window.location.hash=RMC._NOWID;
+				setTimeout(function(){
+				//	$('#'+RMC._NOWID).attr('class','page display-now');
+					$(tmp).attr('class','page hidden');
+				},1200);
+				//var tmp=this._PAGE_STORE.pop();
+				//alert(tmp);
+			}else{
+				if(this._CORDOVA_STATUS){
+					navigator.app.exitApp();
+				}
+			}
 		},
 		//swipe event
 		swipe:function(name,type,fn){
@@ -139,7 +181,23 @@ var RMC={
 		},
 		//cordova執行完成
 		deviceReady:function(){
-			this._CORDOVA_STATUS=true;
+			RMC._CORDOVA_STATUS=true;
+			document.addEventListener("backbutton", RMC.backDown, false);
+		},
+		//執行back按鈕時
+		backDown:function(){
+			RMC.backpage();
+		},
+		//提示訊息
+		alert:function(msg,title,name){
+			if(this._CORDOVA_STATUS){
+				navigator.notification.alert(msg, RMC.alertCallback, title, name);
+			}else{
+				alert(msg);
+			}
+		},
+		alertCallback:function(){
+			
 		},
 		//加入事件
 		add_evnet:function(type,id,fun){
@@ -174,4 +232,7 @@ var RMC={
 //cordova 參數
 $(document).ready(function(){
 	RMC.create();
+	if(RMC._TMP!=''){
+		(RMC._TMP)();
+	}
 });
